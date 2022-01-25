19db50ae06b377d44d43206152f6b8d3a415721c
Sara Perkaj <saraperkaj@pursuit.org>
2022-01-25 11:41:40 -0500

    filling some info for the transactions.

--- models/transaction.js
+++ models/transaction.js
@@ -1,6 +1,18 @@
 const transaction = [
-  { itemName: "", amount: 0, date: "", from: "", category: "" },
-  { itemName: "", amount: 0, date: "", from: "", category: "" },
+  {
+    itemName: "Chipotle",
+    amount: 13.0,
+    date: "january 25, 2022",
+    from: "Chipotle",
+    category: "food",
+  },
+  {
+    itemName: "Prime Phantom",
+    amount: 17.5,
+    date: "10/22/2021",
+    from: "Riot",
+    category: "Video Games",
+  },
 ];
 
 module.exports = transaction;
