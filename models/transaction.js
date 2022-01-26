b77bbf15159194630ce97a82bd5708b0df92f025
Sara Perkaj <saraperkaj@pursuit.org>
2022-01-25 21:51:36 -0500

    minor changes again.

--- models/transaction.js
+++ models/transaction.js
@@ -2,14 +2,14 @@ const transaction = [
   {
     itemName: "Chipotle",
     amount: 13.0,
-    date: "january 25, 2022",
+    date: "January 25,2022",
     from: "Job",
     category: "food",
   },
   {
     itemName: "Prime Phantom",
     amount: 17.5,
-    date: "10/22/2021",
+    date: "October 22,2021",
     from: "Mom",
     category: "Video Games",
   },
